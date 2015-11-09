910d37b07788930bcd23331c286fac78458a9053
ashar7 <asharjaved7@gmail.com>
2015-11-09 23:01:17 +0530

    prediction vector added

--- keras4gist/nn.py
+++ keras4gist/nn.py
@@ -19,23 +19,31 @@ def read_in_chunks(file_object, chunk_size=7):
 
 arr=[]
 arr2=[]
+arr3=[]
 count=0
 count2=0
+count3=0
 f = open('gistFeature.txt')
 for piece in read_in_chunks(f):
-	if count>=20000*2048:
-		if count2>=5000*2048:
-			break
+	if count>=40000*2048:
+		if count2>=2000*2048:
+			if count3>=200*2048:
+				break
+			count3+=1
+			arr3.append(float(piece[0:6]))
+			continue
 		count2+=1
 		arr2.append(float(piece[0:6]))
 		continue
 	count+=1
 	arr.append(float(piece[0:6]))
 
-X_train=np.array(arr).reshape(20000,2048)
-X_test=np.array(arr2).reshape(5000,2048)
-print X_train.shape
-print X_test.shape
+X_train=np.array(arr).reshape(40000,2048)
+X_test=np.array(arr2).reshape(2000,2048)
+X_predict=np.array(arr3).reshape(200,2048)
+print 'Input training vector: ' + str(X_train.shape)
+print 'Input testing vector: ' + str(X_test.shape)
+print 'Input predicting vector: ' + str(X_predict.shape)
 
 
 with open("outVec.json") as inp:
@@ -47,11 +55,12 @@ for i in x:
 	j[i].remove(0)
 	t.append(j[i])
 
-Y_train=np.array(t[0:20000])
-Y_test=np.array(t[20000:25000])
-print Y_train.shape
-print Y_test.shape
-
+Y_train=np.array(t[0:40000])
+Y_test=np.array(t[40000:42000])
+Y_predict=np.array(t[42000:42200])
+print 'Output training vector: ' + str(Y_train.shape)
+print 'Output testing vector: ' + str(Y_test.shape)
+print 'Output predicting vector: ' + str(Y_predict.shape)
 
 
 
@@ -71,5 +80,24 @@ model.compile(loss='categorical_crossentropy', optimizer='adam')
 
 history = model.fit(X_train, Y_train, nb_epoch=nb_epoch, batch_size=batch_size, verbose=1, show_accuracy=True, validation_split=0.1)
 score = model.evaluate(X_test, Y_test, batch_size=batch_size, verbose=1, show_accuracy=True)
+activations = model.predict(X_predict)
+
 print('Test score:', score[0])
 print('Test accuracy:', score[1])
+
+print 'Activations:'
+print activations
+print '\n\n'
+
+print 'Actual results:'
+print t[42000:42200]
+print '\n\n'
+
+temp=[]
+print 'Activations of ON labels:'
+for x,i in enumerate(t[42000:42200]):
+	for y,j in enumerate(i):
+		if j==1:
+			temp.append(activations[x][y])
+
+print temp
